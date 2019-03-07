5f436589d491ee1d3ed7c01adadc8e84c19418e7
strongmuffin <nshusa99@gmail.com>
2019-03-06 20:29:10 -0600

    Changed icons on app drawer

--- src/component/AppDrawer.js
+++ src/component/AppDrawer.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react'
 import Drawer from '@material-ui/core/Drawer'
-import { ListItem, List, ListItemText, Icon } from '@material-ui/core';
+import { ListItem, List, ListItemText } from '@material-ui/core';
 import '../css/AppDrawer.css';
-import { loadCSS } from 'fg-loadcss/src/loadCSS';
 
 const categories = [
-    { id: 1, class: 'fas fa-home', name: 'Home' },
-    { id: 2, class: 'fas fa-trophy', name: 'Ranking' },
-    { id: 3, class: 'fas fa-comment-alt', name: 'Chat' },
-    { id: 4, class: 'fas fa-music', name: 'Music' },
-    { id: 5, class: 'fas fa-video', name: 'Video' },
-    { id: 6, class: 'fas fa-gamepad', name: 'Game' },
-    { id: 7, class: 'fas fa-book', name: 'Reading' }]
+    { id: 1, src: './images/appimage.png', name: 'AppImage' },
+    { id: 2, src: './images/flatpak.png', name: 'Flatpak' },
+    { id: 3, src: './images/snap.png', name: 'Snap' }
+]
 
 function MenuItemList() {
     const listItems = categories.map((item) =>
-        <ListItem button key={item.id.toString()}>
-            <Icon className={item.class} />
+        <ListItem className="list-item" button key={item.id.toString()}>
+            <img className="icon" src={item.src} />
             <ListItemText primary={item.name}></ListItemText>
         </ListItem>
     )
@@ -28,14 +24,6 @@ function MenuItemList() {
 }
 
 export class AppDrawer extends Component {
-
-    componentDidMount() {
-        loadCSS(
-            'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
-            document.querySelector('#insertion-point-jss'),
-        );
-    }
-
     render() {
         return (
             <React.Fragment>
