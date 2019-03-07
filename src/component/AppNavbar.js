6c569ab4d2cc87101c6e9ed3b1fbd4053b70e7ab
strongmuffin <nshusa99@gmail.com>
2019-03-06 21:00:34 -0600

    Added logo to navbar

--- src/component/AppNavbar.js
+++ src/component/AppNavbar.js
@@ -3,13 +3,13 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import '../css/AppNavbar.css';
-import { InputBase } from '@material-ui/core';
 
 export class AppNavbar extends Component {
     render() {
         return (
             <AppBar className="navbar" position="fixed" color="primary">
-                <Toolbar>
+                <Toolbar className="toolbar">
+                  <img className="logo" src={"./images/app_store.png"} style={{width: "32px"}} />
                     <Typography className="brand" variant="h6" color="inherit">
                         App Store
                     </Typography>
