37d0b8946fce141b489420f1bd85bcbe1125d0a1
strongmuffin <nshusa99@gmail.com>
2019-03-06 21:09:03 -0600

    Added hover, entire grid cell clickable

--- src/component/LinuxApp.js
+++ src/component/LinuxApp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import '../css/LinuxApp.css';
 
 const FLATHUB_BASE_URL = 'https://flathub.org'
@@ -9,31 +10,33 @@ const FLATHUB_BASE_URL = 'https://flathub.org'
 export class LinuxApp extends Component {
 
     getDesktopImage(item) {
-      return `${FLATHUB_BASE_URL}${item.iconDesktopUrl}`
+        return `${FLATHUB_BASE_URL}${item.iconDesktopUrl}`
     }
 
     getMobileImage(item) {
         return `${FLATHUB_BASE_URL}${item.iconMobileUrl}`
-      }
+    }
 
     getUrl(item) {
         return `${FLATHUB_BASE_URL}/apps/details/${item.flatpakAppId}`
     }
-  
+
     render() {
-        const { data } = this.props  
+        const { data } = this.props
         return (
             <Card className="card">
-                <Typography className="center title" color="textSecondary" gutterBottom>
-                {data.name}
-                </Typography>
+                <CardActionArea>
+                    <a className="link" href={this.getUrl(data)} target="_blank">
+                        <Typography className="center title" color="textSecondary" gutterBottom>
+                            {data.name}
+                        </Typography>
 
-                <CardContent>
-                    <a href={this.getUrl(data)} target="_blank">
-                        <img className="icon center" src={this.getDesktopImage(data)} />
+                        <CardContent>
+                            <img className="icon center" src={this.getDesktopImage(data)} />
+                        </CardContent>
                     </a>
-                </CardContent>
+                </CardActionArea>
             </Card>
         )
     }
-  }
+}
