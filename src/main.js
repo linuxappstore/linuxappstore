721094aacfefd2a243734564edc666809b0f86cc
Chad Adams <nshusa99@gmail.com>
2019-04-21 23:46:13 -0500

    electron hide menubar

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 })
+  win.setMenuBarVisibility(false)
 
   // and load the index.html of the app.
   win.loadURL('http://localhost:3000')
