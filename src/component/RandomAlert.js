477f960564eea797b85204a7b86cbf011ef2b4ad
chadalen <nshusa99@gmail.com>
2019-04-28 13:44:22 -0500

    Replaced bootstrap alert with react material snackbar

--- src/component/RandomAlert.js
+++ src/component/RandomAlert.js
@@ -1,97 +1,187 @@
-
-import React from 'react';
-import Alert from 'react-bootstrap/Alert'
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import InfoIcon from "@material-ui/icons/Info";
+import classNames from "classnames";
 
 const styles = theme => ({
-    content: {
-        textAlign: 'center'
-    }
-  });
+  content: {
+    textAlign: "center"
+  },
+  info: {
+    backgroundColor: theme.palette.primary.dark
+  },
+  message: {
+    display: "flex",
+    alignItems: "center"
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit
+  },
+  snackBarRoot: {
+    position: "inherit",
+    transform: "inherit"
+  }
+});
 
-const refreshRate = 60
+const refreshRate = 60;
 
 class RandomAlert extends React.Component {
+  state = {
+    timer: 0,
+    msgIdx: 0,
+    open: true
+  };
 
-    state = {
-        timer: 0,
-        msgIdx: 0
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let timer = this.state.timer;
+      if (timer >= refreshRate) {
+        this.resetTimer();
+      } else {
+        this.setState({ timer: timer + 1 });
+      }
+    }, 1000);
+  }
+
+  resetTimer() {
+    let messages = this.messages()
+
+    let pos = Math.floor(Math.random() * messages.length);
+
+    while (pos === this.state.msgIdx) {
+      pos = Math.floor(Math.random() * messages.length)
     }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            let timer = this.state.timer            
-            if (timer >= refreshRate) {
-                this.resetTimer()
-            } else {
-                this.setState({timer: timer + 1});
-            }
-          }, 1000);
-    }
+    this.setState({ timer: 0, msgIdx: pos })
+  }
 
-    resetTimer() {
-        let messages = this.messages()
+  messages() {
+    return [
+      <React.Fragment>
+          Have a bug or want to provide feedback?&nbsp;
 
-        let pos = Math.floor(Math.random()*messages.length)
+        <a
+          href="https://github.com/linuxappstore/linuxappstore-frontend/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        See here
+        </a>
 
-        while(pos === this.state.msgIdx) {
-            pos = Math.floor(Math.random()*messages.length)
-        }
+      </React.Fragment>,
+      <React.Fragment>
+        Release&nbsp;<b>1.0.0</b>&nbsp;coming soon!&nbsp;
+        <a
+          href="https://github.com/linuxappstore/linuxappstore-frontend/milestone/2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See here
+        </a>
 
-        this.setState({timer: 0, msgIdx: pos});
-    }
-
-    messages() {
-        return [
-            (
-            <React.Fragment>
-                    Have a bug or want to provide feedback?
-                    <Alert.Link href="https://github.com/linuxappstore/linuxappstore-frontend/issues" target="_blank"> See here</Alert.Link>
-            </React.Fragment> 
-            ),
-            (
-                <React.Fragment>
-                Release <b>1.0.0</b> coming soon! See progress <Alert.Link href="https://github.com/linuxappstore/linuxappstore-frontend/milestone/2" target="_blank">here</Alert.Link>
-                </React.Fragment>
-            ),
-            (
-                <React.Fragment>
-                    Got an app to submit? See docs (
-                        <Alert.Link href="https://docs.appimage.org/packaging-guide/distribution.html" target="_blank">
-                        AppImage
-                    </Alert.Link>,
-                    <Alert.Link href="https://github.com/flathub/flathub/wiki/App-Submission" target="_blank"> Flatpak
-                    </Alert.Link>,
-                    <Alert.Link href="https://docs.snapcraft.io/releasing-your-app/6795" target="_blank"> Snap
-                    </Alert.Link>
-                    )
-                </React.Fragment>
-
-            ),
-            (
-                <React.Fragment>
-                Want to become a Patron? See <Alert.Link href="https://www.patreon.com/linuxappstore" target="_blank">here</Alert.Link>
-                </React.Fragment>
-            )
-        ]
-    }
-
-    render() {
-        const { classes, style } = this.props
-        return (
-            <Alert variant={"primary"} style={style}>
-                <div className={classes.content}>
-                {this.messages()[this.state.msgIdx]}
-                </div>
-            </Alert>
+      </React.Fragment>,
+      <React.Fragment>
+        Got an app to submit? See docs (
+        <a
+          href="https://docs.appimage.org/packaging-guide/distribution.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AppImage
+        </a>
+        ,
+        &nbsp;
+        <a
+          href="https://github.com/flathub/flathub/wiki/App-Submission"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Flatpak
+        </a>
+        ,
+        &nbsp;
+        <a
+          href="https://docs.snapcraft.io/releasing-your-app/6795"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Snap
+        </a>
         )
-    }
-    
+      </React.Fragment>,
+
+      <React.Fragment>
+        Want to become a Patron?&nbsp;
+        <a href="https://www.patreon.com/linuxappstore" target="_blank" rel="noopener noreferrer">
+          See here
+        </a>
+      </React.Fragment>
+    ];
+  }
+
+  onClose() {
+      this.setState({ open: false })
+  }
+
+  render() {
+    const {
+      classes,
+      className,
+      message,
+      onClose,
+      variant,
+      ...other
+    } = this.props;
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        open={this.state.open}
+        autoHideDuration={6000}
+        className={classNames(classes.snackBarRoot)}
+      >
+        <SnackbarContent
+          className={classNames(classes.info, className)}
+          aria-describedby="client-snackbar"
+          message={
+            <span id="client-snackbar" className={classes.message}>
+              <InfoIcon
+                className={classNames(classes.icon, classes.iconVariant)}
+              />
+              {this.messages()[this.state.msgIdx]}
+            </span>
+          }
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.onClose.bind(this)}
+            >
+              <CloseIcon className={classes.icon} />
+            </IconButton>
+          ]}
+          {...other}
+        />
+      </Snackbar>
+    );
+  }
 }
 
 RandomAlert.propTypes = {
-    classes: PropTypes.object.isRequired
-  };
-  
-  export default withStyles(styles)(RandomAlert);
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(RandomAlert);
